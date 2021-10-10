27ade70baf3efb45657210cb96532a18fa450517
obichidi <innocia2009@gmail.com>
2021-10-10 00:29:01 -0400

    Initial commit

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import PostListView,PostDetailView
+from . import views
+
+
+
+urlpatterns = [
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='blog-home'),
+    path('about/', views.about, name='blog-about'),
+]
---
+++ blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from django.views.generic import ListView
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+
+    }
+    return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
+# Create your views here.
