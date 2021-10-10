8a7b84ad8d19b676e51cd16cdd5ad582615bb505
obichidi <innocia2009@gmail.com>
2021-10-10 02:55:53 -0400

    blog functional

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 
 
 def home(request):
@@ -17,6 +17,11 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
 
 
 def about(request):
